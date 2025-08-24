e6fafce9889ae86f88e9f99c6a0bbc8e5d8a0155
froi6567 <fazeares6567@gmail.com>
2025-08-24 13:07:27 +0100

    ffeffefed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Porttfolio-Web/Project/",
-});
+  base: 'https://froidd.github.io/', // 👈 para repos tipo usuario.github.io
+})
